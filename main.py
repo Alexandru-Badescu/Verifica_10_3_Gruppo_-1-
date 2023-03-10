d6af2e0c57f131b97c36925d8efc0d7474f7fafa
Alexandru-Badescu <alexandru.badescu@polocodigoro.edu.it>
2023-03-10 09:21:28 -0200

    Verifica_10_3_Gruppo_-1-!

---
+++ main.py
@@ -0,0 +1,9 @@
+def Verifica_10_3_Gruppo_1(nome, cognome):
+    """Restituisce una stringa di saluto con nome e cognome"""
+    return f"Ciao {nome} {cognome}, piacere di conoscerti!"
+
+# Esempio di utilizzo della funzione saluto_cognome()
+nome_utente = input("Inserisci il tuo nome: ")
+cognome_utente = input("Inserisci il tuo cognome: ")
+saluto = Verifica_10_3_Gruppo_1(cognome_utente,nome_utente)
+print(saluto)
