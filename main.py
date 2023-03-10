b6bee0abe49962cbed7da99299782c02864fef79
Alexandru-Badescu <104421993+Alexandru-Badescu@users.noreply.github.com>
2023-03-10 10:15:46 -0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import Verifica_10_3_Gruppo_1(Saluto_Carli_Gruppo1)
+import Verifica_10_3_Gruppo_1(saluto_Rolfini_Gruppo_1)
+import Verifica_10_3_Gruppo_1(saluto_Mantovani_Gruppo1)
+import Verifica_10_3_Gruppo_1(simonevecchiattini)
 def Verifica_10_3_Gruppo_1(nome, cognome):
     """Restituisce una stringa di saluto con nome e cognome"""
     return f"Ciao {nome} {cognome}, piacere di conoscerti!"
